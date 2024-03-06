ab8f6c2d807bf5596c13b9e2995491568b524978
GabrielFerreira <gaferreira.work@gmail.com>
2024-03-06 12:58:29 +0000

    End of HO2

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -6,47 +6,29 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 
 class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    # Other fields as needed
 
-	COUNTRY_CHOICES = [
-	('US', 'United States'),
-	('UK','United KIngdom'),
-	('DE', 'Germany'),
-	('FR', 'France'),
-	('ES', 'Spain'),
-	('SWE', 'Sweden'),
-	('CHN', 'China'),
-	('JPN','Japan'),
-	('KRN', 'South-Korea'),
-	('IT','Italy'),
-	]
+    def __str__(self):
+        return self.name  # Return the name as the string representation
 
-	name = models.CharField(max_length=100)
-	description = models.TextField()
-	madeIncountry = models.CharField(max_length=3, choices=COUNTRY_CHOICES)
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    name = models.CharField(max_length=100)
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        # Add more choices as required
+    ]
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
+    year = models.IntegerField(default=2023,
+        validators=[
+            MaxValueValidator(2023),
+            MinValueValidator(2015)
+        ])
+    # Other fields as needed
 
-	def __str__(self):
-		return self.name # Return the name as the string representation
-
-class CarModel(models.Model): 
-	
-	TYPE_CHOICES = [
-	('SEDAN','Sedan'),
-	('SUV', 'SubUrban Vehicle'),
-	('CABRIO', 'Cabriolet'),
-	('SW', 'Station Wagon'),
-	('COUPE', 'coupe'),
-	('LIMO', 'Limousine'),
-	]
-
-	make = models.ForeignKey(CarMake, on_delete=models.CASCADE) #Many-to-one relationship
-	dealerId = models.IntegerField()
-	name = models.CharField(max_length=150)
-	type = models.CharField(max_length=6, choices=TYPE_CHOICES)
-	year = models.IntegerField(default=2024,
-		validators=[
-			MaxValueValidator(2024),
-			MinValueValidator(1980)
-		])
-
-	def __srt__(self):
-		return self.make, self.name
+    def __str__(self):
+        return self.name  # Return the name as the string representation
